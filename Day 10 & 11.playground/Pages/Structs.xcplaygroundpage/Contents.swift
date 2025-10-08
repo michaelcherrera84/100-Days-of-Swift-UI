9dd28c563ebf66faebf67f1b483054a388817b05
Michael C. Herrera <04.ideas-amici@icloud.com>
2025-10-07 22:28:48 -0500

    Add Day 10 & 11

---
+++ Day 10 & 11.playground/Pages/Structs.xcplaygroundpage/Contents.swift
@@ -0,0 +1,181 @@
+import Cocoa
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+    
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+
+struct Employee {
+    let name: String
+    var vacationRemaining = 14
+    
+    mutating func takeVacation(days: Int) {
+        if vacationRemaining > days {
+            vacationRemaining -= days
+            print("I'm going on vations!")
+            print("Days remaining: \(vacationRemaining)")
+        } else {
+            print("Oops! there aren't enough days remaining.")
+        }
+    }
+}
+
+var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)  // cannot be let archer...
+archer.takeVacation(days: 5)
+print(archer.vacationRemaining)
+
+let kane = Employee(name: "Lana Kane")
+let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
+
+
+/// How to compute property values dynamically
+
+struct Employee1 {
+    let name: String
+    var vacationAllocated = 14
+    var vactionTaken = 0
+    
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vactionTaken
+        }
+        
+        set {
+            vacationAllocated = vactionTaken + newValue
+        }
+    }
+}
+
+var archer1 = Employee1(name: "Sterling Archer", vacationAllocated: 14)
+archer1.vactionTaken += 4
+archer1.vacationRemaining = 5
+print(archer1.vacationAllocated)
+
+
+/// How to take action when a property changes
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("there are now \(contacts.count) contacts")
+            print("Old value was: \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+
+/// How to create custom initializers
+
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
+
+
+/// How to limit access to internal data using access control
+
+struct BankAccount {
+//    private var funds = 0  // or
+    private(set) var funds = 0
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds > amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
+
+
+/// Static properties and methods
+
+struct School {
+    static var studentCount = 0
+    
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+School.add(student: "Taylor Swift")
+print(School.studentCount)
+
+
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "settings.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
+struct Employee2 {
+    let username: String
+    let password: String
+    
+    static let example = Employee2(username: "cfederighi", password: "h4irf0rce0ne")
+}
+
+
+Employee2.example
