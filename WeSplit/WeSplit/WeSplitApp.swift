4b541ebeadba5d2e5253f0b72de64a6b4a0d35b5
Michael C. Herrera <michael.c.herrera@icloud.com>
2025-10-10 13:56:32 -0500

    Add Day 16 - 18

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Michael C. Herrera on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
