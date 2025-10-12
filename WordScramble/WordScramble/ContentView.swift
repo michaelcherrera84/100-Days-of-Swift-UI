c300de26562c31c298771fd98b5f644fc934ce49
Michael C. Herrera <04.ideas-amici@icloud.com>
2025-10-12 16:53:38 -0500

    Add Day 29 - 31

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,159 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Michael C. Herrera on 10/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    private var totalLetters: Int {
+        var letters = 0
+        for word in usedWords {
+            letters += word.count
+        }
+        return letters
+    }
+
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+            } message: {
+                Text(errorMessage)
+            }
+            .toolbar {
+                Button("New Game") {
+                    startGame()
+                }
+            }
+            
+            Spacer()
+            
+            VStack {
+                VStack {
+                    Text("Total letters used: **\(totalLetters)**")
+                }
+                .padding(10)
+                .frame(maxWidth: .infinity)
+                .overlay(RoundedRectangle(cornerRadius: 15).stroke(lineWidth: 2))
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(
+            in: .whitespacesAndNewlines
+        )
+
+        guard answer.count > 0 else { return }
+
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word already used", message: "Each word must be unique!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "That word cannot be made with the letters in the root word.")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not in dictionary", message: "\(answer) is not a recognized word.")
+            return
+        }
+
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+
+    func startGame() {
+        usedWords.removeAll()
+        if let startWordsURL = Bundle.main.url(
+            forResource: "start",
+            withExtension: "txt"
+        ) {
+            if let startWords = try? String(
+                contentsOf: startWordsURL,
+                encoding: .utf8
+            ) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+
+        fatalError("Could not load start.txt from bundle.")
+    }
+
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(
+            in: word,
+            range: range,
+            startingAt: 0,
+            wrap: false,
+            language: "en"
+        )
+
+        return misspelledRange.location == NSNotFound
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Michael C. Herrera on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
