7af63bf548d300dec341a0c0d154676beb1dcff9
Michael C. Herrera <michael.c.herrera@icloud.com>
2025-10-12 21:12:43 -0500

    Add Day 32 - 34

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Michael C. Herrera on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
