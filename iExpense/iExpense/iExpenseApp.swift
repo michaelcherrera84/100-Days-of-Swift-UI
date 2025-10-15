a8bce1e32de4f4073e0f7e23c5e0b7ba7fe231b5
Michael C. Herrera <michael.c.herrera@icloud.com>
2025-10-14 21:29:44 -0500

    Add Day 36 - 38

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Michael C. Herrera on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
