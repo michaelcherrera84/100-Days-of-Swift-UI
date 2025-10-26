592a736c8f6152b697055c15d3186d3c62d75672
Michael C. Herrera <04.ideas-amici@icloud.com>
2025-10-26 12:43:54 -0500

    Add Day 61

--- FriendFace/FriendFace/Friend.swift
+++ FriendFace/FriendFace/Friend.swift
@@ -5,9 +5,30 @@
 //  Created by Michael C. Herrera on 10/24/25.
 //
 
+import SwiftData
 import Foundation
 
-struct Friend: Codable, Identifiable, Hashable {
+@Model
+class Friend: Codable, Identifiable {
+    enum CodingKeys: CodingKey { case id, name }
+
     var id: UUID
     var name: String
+
+    init(id: UUID = UUID(), name: String) {
+        self.id = id
+        self.name = name
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        name = try container.decode(String.self, forKey: .name)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(name, forKey: .name)
+    }
 }
