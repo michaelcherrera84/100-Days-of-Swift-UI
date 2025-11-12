03f76687e1319812b96995b54f008d69466390b6
Michael C. Herrera <04.ideas-amici@icloud.com>
2025-11-11 19:28:44 -0600

    Add Day 77-78

---
+++ NameThatPhoto/NameThatPhoto/LocationFetcher.swift
@@ -0,0 +1,28 @@
+//
+//  LocationFetcher.swift
+//  NameThatPhoto
+//
+//  Created by Michael C. Herrera on 11/11/25.
+//
+
+import CoreLocation
+import Foundation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
